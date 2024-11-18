1b7b58f4cd1f9b563e1e8db65cb85ca083b96b47
Gabriel <gabriel.oliver77@gmail.com>
2024-11-18 11:50:38 +0100

    añadido carpetas

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+"use strict";
 const express = require('express');
 const path = require('path');
+const morgan = require("morgan");
 const app = express();
 
 const mysql = require('mysql');
@@ -15,6 +17,13 @@ const pool = mysql.createPool({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json()); // support json encoded bodies
+
 
 app.use('/', routes);
 
