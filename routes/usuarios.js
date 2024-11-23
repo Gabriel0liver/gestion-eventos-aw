72c33d6d7866e6f1da8fd5d28f06e400b09a2d40
Gabriel <gabriel.oliver77@gmail.com>
2024-11-23 16:58:46 +0100

    eliminado controlador

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,10 +4,53 @@ const userRouter = express.Router();
 
 const userController = new UserController();
 
-userRouter.post("/login", userController.loginPost);
+userRouter.post("/login", (req,res,next) =>{
 
-userRouter.post("/register", userController.registerPost);
+    let user = {
+        nombre: req.body.nombre,
+        correo: req.body.correo,
+        contrasena: req.body.telefono,
+        facultad: req.body.facultad,
+        rol: req.body.rol
+    }
 
-userRouter.post("/recuperar",userController.recuperarPost);
+    daoU.insertUser(user, function (error, usuario) {
+            if (error) {
+                console.log(error);
+                next();
+            }
+            else {
+                req.session.currentUser = req.body.correo;
+                res.status(200).redirect("/usuarios/home");
+
+            }
+        });
+});
+
+userRouter.post("/register", (req,res,next)=>{
+    daoU.isUserCorrect(req.body.correo, req.body.password,
+        (error, userCorrect) => {
+            if (error) { // error de acceso a la base de datos
+                res.status(500);
+                res.render("login",
+                    { errorMsg: error.message });
+            }
+            else if (userCorrect) {
+
+                req.session.currentUser = req.body.correo;
+
+                res.redirect("/usuarios/home");
+            }
+            else {
+                res.status(200);
+                res.render("login",
+                    { errorMsg: error.message });
+            }
+        });
+});
+
+userRouter.post("/recuperar",()=>{
+
+});
 
 module.exports= userRouter;
